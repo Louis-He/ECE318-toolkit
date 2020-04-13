39fe692d5c4a3c04265c179e196dc12e1f3d40a2
Louis-He <1726110778@qq.com>
2020-04-13 03:05:37 -0400

    finish fresnal coef command

--- color.py
+++ color.py
@@ -3,5 +3,45 @@ terminalcolors = {"OKGREEN": '\033[92m',
                   "FAIL": '\033[91m',
                   "ENDC": '\033[0m'}
 
+cmdList = {
+    "fr": {"args": ["ni", "nt", "theta_i"], "types": ["float", "float", "float"], "desc": "Find fresnal coef."},
+    "q/quit": {"args": ["NONE"], "desc": "Quit the toolkit."},
+    "help": {"args": ["NONE"], "desc": "Show this help message."}
+}
+
 def print_to_terminal(type, text):
     print(terminalcolors[type] + text + terminalcolors["ENDC"])
+
+def print_args_error(cmd):
+    arguments_needed = ""
+
+    for i in cmdList[cmd]["args"]:
+        arguments_needed += i + ", "
+    arguments_needed = arguments_needed[: -2]
+    arguments_needed += "."
+    print(terminalcolors["FAIL"] + cmd + " needs " + str(len(cmdList[cmd]["args"])) + " arguments: " + arguments_needed + terminalcolors["ENDC"])
+
+def print_type_error(cmd):
+    type_needed = ""
+
+    for i in cmdList[cmd]["types"]:
+        type_needed += i + ", "
+    type_needed = type_needed[: -2]
+    type_needed += "."
+    print(terminalcolors["FAIL"] + cmd + " needs " + str(len(cmdList[cmd]["args"])) + " arguments with type: " + type_needed + terminalcolors["ENDC"])
+
+def printhelp(cmd):
+    if len(cmd) == 1:
+        print_to_terminal("OKGREEN", "ECE318 toolkit now support the following commands:")
+        for cmd in cmdList:
+           print(cmd.ljust(10), cmdList[cmd]["desc"][:-1].ljust(50), "Required Arguments:", cmdList[cmd]["args"])
+    else:
+        printhelp_cmd(cmd)
+
+def printhelp_cmd(cmd):
+    if(len(cmd) > 2):
+        print(terminalcolors["FAIL"] + "Help can only have zero or one argument." + terminalcolors["ENDC"])
+    try:
+        print(cmd[1].ljust(10), cmdList[cmd[1]]["desc"][:-1].ljust(50), "Required Arguments:", cmdList[cmd[1]]["args"])
+    except:
+        print_to_terminal("FAIL", "command not found: " + cmd[1])
--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def get_refractive(ni, nt, theta_i):
 #               if 'T', return list: [phi_te, phi_tm]
 #               if 'N', return list: [r_te, t_te, r_tm, t_tm]
 #
-#   todo: test this function
 def get_fresnal_coef(ni, nt, theta_i):
     theta_t = get_refractive(ni, nt, theta_i)
 
@@ -34,21 +33,54 @@ def get_fresnal_coef(ni, nt, theta_i):
         phi_te = -2 * np.arctan((nt * np.sqrt(np.power(ni * np.sin(theta_i) / nt, 2) - 1)) / (ni * np.cos(theta_i)))
         phi_tm = -2 * np.arctan((ni * np.sqrt(np.power(ni * np.sin(theta_i) / nt, 2) - 1)) / (nt * np.cos(theta_i))) + np.pi
 
-        return ('T', [phi_te, phi_tm])
+        return ('T', [round(phi_te, 4), round(phi_tm, 4)])
     else:
         r_te = (ni * np.cos(theta_i) - nt * np.cos(theta_t)) / (ni * np.cos(theta_i) + nt * np.cos(theta_t))
         r_tm = (2 * ni * np.cos(theta_i)) / (ni * np.cos(theta_i) + nt * np.cos(theta_t))
         t_te = (-nt * np.cos(theta_i) + ni * np.cos(theta_t)) / (nt * np.cos(theta_i) + ni * np.cos(theta_t))
         t_tm = (2 * ni * np.cos(theta_i)) / (nt * np.cos(theta_i) + ni * np.cos(theta_t))
 
-        return ('N', [r_te, r_tm, t_te, t_tm])
+        return ('N', [round(r_te, 4), round(r_tm, 4), round(t_te, 4), round(t_tm, 4)])
 
-cmdList = [
-    'fresnal'
-]
+def cmd_fresnal(cmd):
+    # check args length
+    if len(cmd) != 4:
+        print_args_error(cmd[0])
+        return
+
+    # check args type
+    ni, nt, theta_i = cmd[1:]
+    try:
+        ni = float(ni)
+        nt = float(nt)
+
+        # support both radius and degree
+        if len(theta_i) > 3 and theta_i[-3:] == 'deg':
+            theta_i = float(theta_i[:-3])
+            theta_i = deg_2_rad(theta_i)
+        else:
+            theta_i = float(theta_i)
+
+    except:
+        print_type_error("fr")
+        return -1
+
+    # command start here
+    res = get_fresnal_coef(ni, nt, theta_i)
+    if (res[0] == 'T'):
+        print_to_terminal("WARNING", "Total Internal Reflection")
+        print('phi_te: ', res[1][0], '=', round(rad_2_deg(res[1][0]), 3), 'deg\t\tphi_tm: ', res[1][1], '=', round(rad_2_deg(res[1][1]), 3), 'deg')
+    else:
+        print('r_te: ', res[1][0], '\t\tt_te: ', res[1][1])
+        print('r_tm: ', res[1][2], '\t\tt_tm: ', res[1][3])
+
+
 if __name__ == '__main__':
     print_to_terminal("OKGREEN", "ECE318 toolkit version 0.1")
     print("source code avaliable @ https://github.com/Louis-He/ECE318-toolkit\n")
+    print_to_terminal("WARNING",
+                      "Degree supported by adding deg to the end, eg. 30deg.\nOtherwise, the toolkit will regard all your inputs as in radius.")
+    print("Type \"help\" to know more about supported commands.\n")
     while (True):
         cmd = input("ECE318_toolkit >> ")
         cmd = cmd.split()
@@ -56,6 +88,14 @@ if __name__ == '__main__':
         if len(cmd) <= 0:
             continue
 
+        if cmd[0] == "quit" or cmd[0] == "q":
+            exit(0)
+
         if cmd[0] not in cmdList:
             print_to_terminal("FAIL", "command not found: " + cmd[0])
             continue
+
+        if cmd[0] == "help":
+            printhelp(cmd)
+        elif cmd[0] == "fr":
+            cmd_fresnal(cmd)
